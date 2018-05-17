e919aac26e9848df9a50b27f695f6b01db564900
Jerry Chai <jchai002@gmail.com>
2018-05-17 16:22:06 -0700

    add more tests

--- test/token.test.js
+++ test/token.test.js
@@ -6,8 +6,9 @@ const TestToken = artifacts.require("./TestToken.sol");
 
 // note: testrpc should have at least 600 accounts (exclude owner) for the user mint tests
 contract("Contract: TestToken", accounts => {
-  const owner = accounts[0];
   var token;
+  const ownerMintLimit = ether(100000);
+  const selfMintLimit = ether(10000);
 
   describe("owner minting", function() {
     beforeEach(async () => {
@@ -15,29 +16,29 @@ contract("Contract: TestToken", accounts => {
     });
 
     it("attempt to mint while under owner mint limits should succeed", async () => {
-      // var tokenBalance;
-      // // mint 100k token to each of the first 60 accounts
-      // for (var i = 1; i <= 60; i++) {
-      //   await token.ownerMint(accounts[i], ether(100000));
-      //   tokenBalance = await token.balanceOf(accounts[i]);
-      //   assert.equal(tokenBalance.toNumber(), ether(100000).toNumber());
-      // }
+      var tokenBalance;
+      // mint 100k token to each of the first 60 accounts
+      for (var i = 1; i <= 60; i++) {
+        await token.ownerMint(accounts[i], ownerMintLimit);
+        tokenBalance = await token.balanceOf(accounts[i]);
+        expect(tokenBalance).to.deep.equal(ownerMintLimit);
+      }
     });
 
     it("attempt to mint while over total owner mint limit should fail", async () => {
-      // // mint 100k token to each of the first 60 accounts
-      // for (var i = 1; i <= 60; i++) {
-      //   await token.ownerMint(accounts[i], ether(100000));
-      // }
-      // // try to mint 1 more wei to account 61
-      // await assertRevert(token.ownerMint(accounts[61], 1));
+      // mint 100k token to each of the first 60 accounts
+      for (var i = 1; i <= 60; i++) {
+        await token.ownerMint(accounts[i], ownerMintLimit);
+      }
+      // try to mint 1 more wei to account 0
+      await assertRevert(token.ownerMint(accounts[0], 1));
     });
 
     it("attempt to mint while over per-user owner mint limit should fail", async () => {
-      // // first mint to 100k limit
-      // await token.ownerMint(accounts[1], ether(100000));
-      // // then try to mint 1 more wei
-      // await assertRevert(token.ownerMint(accounts[1], 1));
+      // first mint to 100k limit
+      await token.ownerMint(accounts[1], ownerMintLimit);
+      // then try to mint 1 more wei
+      await assertRevert(token.ownerMint(accounts[1], 1));
     });
   });
 
@@ -46,7 +47,7 @@ contract("Contract: TestToken", accounts => {
       token = await TestToken.new();
       // mint 6mil tokens from owner
       for (var i = 1; i <= 60; i++) {
-        await token.ownerMint(accounts[i], ether(100000));
+        await token.ownerMint(accounts[i], ownerMintLimit);
       }
     });
 
@@ -54,21 +55,30 @@ contract("Contract: TestToken", accounts => {
       var tokenBalance;
       // mint 10k token to each of the first 600 accounts
       for (var i = 1; i <= 600; i++) {
-        await token.selfMint(ether(10000), { from: accounts[i] });
+        await token.selfMint(selfMintLimit, { from: accounts[i] });
         tokenBalance = await token.balanceOf(accounts[i]);
         if (i <= 60) {
           // the first 60 accounts should have 110k tokens
-          assert.equal(tokenBalance.toNumber(), ether(110000).toNumber());
+          expect(tokenBalance).to.deep.equal(ether(110000));
         } else {
           // the test should have 10k tokens
-          assert.equal(tokenBalance.toNumber(), ether(10000).toNumber());
+          expect(tokenBalance).to.deep.equal(selfMintLimit);
         }
       }
     });
 
-    it("attempt to mint while over total user mint limit should fail", async () => {});
+    it("attempt to mint while over total user mint limit should fail", async () => {
+      // mint 10k token to each of the first 600 accounts
+      for (var i = 1; i <= 600; i++) {
+        await token.selfMint(selfMintLimit, { from: accounts[i] });
+      }
+      await assertRevert(token.selfMint(1, { from: accounts[0] }));
+    });
 
-    it("attempt to mint while over per-user user mint limit should fail", async () => {});
+    it("attempt to mint while over per-user user mint limit should fail", async () => {
+      await token.selfMint(selfMintLimit, { from: accounts[0] });
+      await assertRevert(token.selfMint(1, { from: accounts[0] }));
+    });
   });
 
   describe("transfer", function() {
@@ -76,8 +86,24 @@ contract("Contract: TestToken", accounts => {
       token = await TestToken.new();
     });
 
-    it("transfer should fail before all minting complete", async () => {});
+    it("transfer should fail before all minting complete", async () => {
+      await token.ownerMint(accounts[1], ownerMintLimit);
+      await assertRevert(token.transfer(accounts[2], 1, { from: accounts[1] }));
+    });
 
-    it("transfer should succeed after all minting complete", async () => {});
+    it("transfer should succeed after all minting complete", async () => {
+      // mint 100k token to each of the first 60 accounts
+      for (var i = 1; i <= 60; i++) {
+        await token.ownerMint(accounts[i], ownerMintLimit);
+      }
+      // mint 10k token to each of the first 600 accounts
+      for (var i = 1; i <= 600; i++) {
+        await token.selfMint(selfMintLimit, { from: accounts[i] });
+      }
+      var account2balanceBefore = await token.balanceOf(accounts[2]);
+      await token.transfer(accounts[2], 1, { from: accounts[1] });
+      var account2balanceAfter = await token.balanceOf(accounts[2]);
+      expect(account2balanceBefore.plus(1)).to.deep.equal(account2balanceAfter);
+    });
   });
 });
